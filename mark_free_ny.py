bd5212ae7105e78b47f66449b98b997b0441501f
leurd <leurd@qq..com>
2019-04-28 19:55:51 +0800

    可以判断做种数和时间啦    nanyang测试通过

--- mark_free_ny.py
+++ mark_free_ny.py
@@ -19,7 +19,7 @@ def get_info(PageLink,cookies,sizerule):
                 return {}
             continue
     torrentinfo=re.finditer('<table class="torrentname" width="100%">(.+?)<td class="rowfollow nowrap" valign="middle"',html,re.S)
-    maxfind = 90
+    maxfind = 2
     nowfind = 0
     res = {}
     for p in torrentinfo:
@@ -32,10 +32,12 @@ def get_info(PageLink,cookies,sizerule):
         tor_youhui=tor_youhui.group()[11:-1] if tor_youhui else 'None'
         tor_star = re.search('javascript: bookmark(.+?)bookmark',info_all).group()
         tor_star  = True if 'delbookmark' not in tor_star else False
-        tor_size =  re.search('<td class="rowfollow nowrap">(.+?)<td class="rowfollow">(.+?)<td class="rowfollow"',info_all).group()
+        tor_size =  re.search('<td class="rowfollow nowrap">(.+?)<td class="rowfollow">(.+?)</td>(.+?)</td>',info_all).group()
+        tor_seed =  0 if 'red' in tor_size else int(re.findall('seeders">(.+?)</a>',tor_size)[0])
+        tor_time =  ' '.join(re.findall('<td class="rowfollow nowrap">(.+?)<br />(.+?)</td>',tor_size)[0])
         tor_size =  re.findall('class="rowfollow">(.+?)<br />(.+?)</td>',tor_size)[0]
         tor_size = float(tor_size[0])*10**(sizerule.index(tor_size[1])-2)
-        res[tor_id] = [tor_star,tor_youhui,tor_size,tor_title]
+        res[tor_id] = [tor_star,tor_youhui,tor_size,tor_title,tor_time,tor_seed]
         nowfind+=1
         if nowfind == maxfind :break
     return res
@@ -56,7 +58,7 @@ if __name__ == "__main__":
     headers= {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
                   'cookie':cookies}
     for tor_id in list(tor_info):
-        if  not tor_info[tor_id][0] and  sizefilter[0]<tor_info[tor_id][2]<sizefilter[1] and tor_info[tor_id][1] =='free':
+        if  not tor_info[tor_id][0] and  sizefilter[0]<tor_info[tor_id][2]<sizefilter[1] and tor_info[tor_id][1] =='free' and tor_info[tor_id][5]:
             print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())+'  '+tor_info[tor_id][3])
             maxtry = 4
             for tyies in range(1,maxtry):
@@ -67,6 +69,5 @@ if __name__ == "__main__":
                     print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())+'  '+'网络连接失败，重试第'+str(tyies)+'次中')
                     if tyies == 3:
                         print('GG')
-                    continue            
-
+                    continue    
 
